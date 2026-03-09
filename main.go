39297b96347bd0a7ca7fea7df34fb2cd7d03420a
Keegan Fontaine <keegsfontaine@gmail.com>
2026-03-09 19:40:55 -0400

    Add handlers and ds to send messages to rooms

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/websocket"
 )
 
+type Message struct {
+	From    *websocket.Conn
+	Content string
+}
+
 type Room struct {
-	Name string
+	Name     string
+	Messages []Message
 }
 
 var upgrader = websocket.Upgrader{
@@ -17,7 +25,24 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+func viewRoom(r *Room) []string {
+	formattedMessages := []string{fmt.Sprintf("ROOM %s\n\n------------\n\n", r.Name)}
+
+	for _, m := range r.Messages {
+		formattedMessages = append(formattedMessages,
+			fmt.Sprintf("========\nNAME: %s\nMESSAGE: %s\n========\n", m.From.RemoteAddr(), m.Content),
+		)
+	}
+
+	return formattedMessages
+}
+
 func main() {
+	rooms := []Room{
+		{Name: "first", Messages: []Message{}},
+		{Name: "second", Messages: []Message{}},
+	}
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
@@ -25,13 +50,45 @@ func main() {
 		}
 
 		for {
-			msgType, message, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("Err! Couldn't read message", err)
+			var message struct {
+				RoomId  int
+				Message string
 			}
 
-			fmt.Println(msgType, string(message))
+			if err := conn.ReadJSON(&message); err != nil {
+				log.Println("Err! Couldn't parse JSON message!", err)
+			}
+
+			room := &rooms[message.RoomId]
+
+			room.Messages = append(room.Messages, Message{
+				From:    conn,
+				Content: message.Message,
+			})
+
+			if err := conn.WriteJSON(map[string]int{"apple": 5, "lettuce": 7}); err != nil {
+				log.Println("Err! Couldn't write JSON message!", err)
+			}
 		}
+	})
+
+	http.HandleFunc("/view/{id}", func(w http.ResponseWriter, r *http.Request) {
+		roomPath := r.PathValue("id")
+		roomIndex, err := strconv.Atoi(roomPath)
+
+		fmt.Println(rooms)
+
+		if err != nil {
+			log.Println("Err! Couldn't get roomIndex", err)
+			w.Write([]byte("Invalid Room ID"))
+		}
+
+		room := rooms[roomIndex]
+		full := strings.Join(viewRoom(&room), "")
+
+		w.Write(
+			[]byte(full),
+		)
 	})
 
 	fmt.Println("Server starting...")
